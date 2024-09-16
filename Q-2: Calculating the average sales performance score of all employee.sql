16e6b7e10b55d4dc1b645789e51b84f7ad09fe22
Md. Bakhtiar Galib Siyam <106044768+siyam97cse@users.noreply.github.com>
2024-09-16 12:52:12 +0600

    Create Q-2: Calculating the average sales performance score of all employee.sql

---
+++ Q-2: Calculating the average sales performance score of all employee.sql
@@ -0,0 +1,55 @@
+-- Task: Lab Aggregation Function
+-- Submitted by: Md. Bakhtiar Galib Siyam
+-- Main Batch: 54(A)
+-- Continued Batch: 55(A) 
+-- Student_ID: 2212081040
+
+-- Question 2: Write an SQL query to calculate the average sales performance score of all employees.
+
+-- Anster to the Question No. 02:
+
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(100)
+);
+
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    employee_name VARCHAR(100),
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+CREATE TABLE performance_reviews (
+    review_id INT PRIMARY KEY,
+    employee_id INT,
+    review_date DATE,
+    sales_performance_score INT,  
+    customer_feedback_score INT,  
+    project_completion_score INT, 
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+);
+
+INSERT INTO departments (department_id, department_name)
+VALUES (1, 'Finance'), (2, 'IT'), (3, 'Transport'), (4, 'Marketing'), (5, 'Quality_Assurance');
+
+INSERT INTO employees (employee_id, employee_name, department_id)
+VALUES (1, 'Bakhtiar', 1), (2, 'Galib', 2), (3, 'Siyam', 3), (4, 'Tamim', 4), (5, 'Tanik', 5);
+
+INSERT INTO performance_reviews (review_id, employee_id, review_date, sales_performance_score, customer_feedback_score, project_completion_score)
+VALUES 
+(1, 1, '2022-08-16', 9, 7, 3),
+(2, 2, '2020-09-17', 7, 8, 9),
+(3, 3, '2023-09-18', 6, 2, 5),
+(4, 4, '2023-09-19', 5, 4, 8),
+(5, 5, '2024-04-13', 8, 9, 6),
+(6, 1, '2024-05-14', 7, 8, 0),
+(7, 2, '2024-11-15', 3, 5, 8),
+(8, 3, '2024-04-16', 6, 9, 9),
+(9, 3, '2024-05-16', 4, 5, 7),
+(10, 3, '2024-02-16', 6, 3, 9);
+
+
+-- This is the main part of the code which calculates the average sales performance score of all employees.
+SELECT AVG(sales_performance_score) AS avg_sales_score
+FROM performance_reviews;
