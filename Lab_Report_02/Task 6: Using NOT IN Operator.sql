a5e030dab9c7426d6d0278c291ebe5119ebc6296
Md. Bakhtiar Galib Siyam <106044768+siyam97cse@users.noreply.github.com>
2024-11-06 21:52:49 +0600

    Create Task 6: Using NOT IN Operator.sql

---
+++ Lab_Report_02/Task 6: Using NOT IN Operator.sql
@@ -0,0 +1,29 @@
+-- Lab Report 06
+
+-- Submitted by: Md. Bakhtiar Galib Siyam
+-- Main Batch: 54(A)
+-- Continued Batch: 55(A) 
+-- Student_ID: 2212081040
+
+-- Task 6: Using NOT IN Operator
+
+-- Solution:
+CREATE TABLE orders12 (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    amount INT
+);
+
+INSERT INTO orders12 (order_id, customer_id, order_date, amount) VALUES
+(1, 1, '2024-11-01', 150),
+(2, 2, '2024-11-02', 200),
+(3, 3, '2024-11-03', 300),
+(4, 4, '2024-11-04', 450),
+(5, 5, '2024-11-05', 120);
+
+SELECT *
+FROM orders12
+WHERE customer_id NOT IN (1, 2, 3);
+
+
